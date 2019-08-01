57decddcafacc68b36ebd4bb8bd6b6eeae1c8a2c
Bruno Lopes <brunomlopes@gmail.com>
2019-08-02 00:00:58 +0100

    Updated to netcore 2.2

--- build.ps1
+++ build.ps1
@@ -9,20 +9,20 @@ if (-not (Test-Path local)) {
 dotnet publish -c Release -r $rid /p:NativeCompilationDuringPublish=false 
 
 if($rid -eq "win-x64"){
-    Copy-Item ".\NugetVendor\bin\Release\netcoreapp2.0\$rid\publish\clrcompression.dll" local\clrcompression.dll -Verbose -Force
+    Copy-Item ".\NugetVendor\bin\Release\netcoreapp2.2\$rid\publish\clrcompression.dll" local\clrcompression.dll -Verbose -Force
 }
 
 dotnet publish -c Release -r $rid
 
 if($rid -eq "win-x64"){
-    Copy-Item  local\clrcompression.dll ".\NugetVendor\bin\Release\netcoreapp2.0\$rid\native\clrcompression.dll" -Verbose -Force
+    Copy-Item  local\clrcompression.dll ".\NugetVendor\bin\Release\netcoreapp2.2\$rid\native\clrcompression.dll" -Verbose -Force
 }
 
 $archive_files = @()
 if($rid -eq "win-x64"){
-    $archive_files = @(".\NugetVendor\bin\Release\netcoreapp2.0\$rid\native\NugetVendor.exe",".\NugetVendor\bin\Release\netcoreapp2.0\$rid\native\clrcompression.dll")
+    $archive_files = @(".\NugetVendor\bin\Release\netcoreapp2.2\$rid\native\NugetVendor.exe",".\NugetVendor\bin\Release\netcoreapp2.2\$rid\native\clrcompression.dll")
 }else{
-    $archive_files = @(".\NugetVendor\bin\Release\netcoreapp2.0\$rid\native\NugetVendor")
+    $archive_files = @(".\NugetVendor\bin\Release\netcoreapp2.2\$rid\native\NugetVendor")
 }
 
 Compress-Archive -Path $archive_files -DestinationPath ".\local\NugetVendor.$rid.zip" -Force -Verbose
