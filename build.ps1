4bb56ac596b912e678084fb67377f90cdc63cdf5
Bruno Lopes <brunomlopes@gmail.com>
2019-08-02 11:44:49 +0100

    I think this now creates a valid package with latest releases

--- build.ps1
+++ build.ps1
@@ -6,21 +6,12 @@ if (-not (Test-Path local)) {
     new-item local -ItemType Directory -Force
 }
 dotnet restore
-dotnet publish -c Release -r $rid /p:NativeCompilationDuringPublish=false /p:CoreRT=false
-
-if($rid -eq "win-x64"){
-    Copy-Item ".\NugetVendor\bin\Release\netcoreapp2.2\$rid\publish\clrcompression.dll" local\clrcompression.dll -Verbose -Force
-}
 
 dotnet publish -c Release -r $rid
 
-if($rid -eq "win-x64"){
-    Copy-Item  local\clrcompression.dll ".\NugetVendor\bin\Release\netcoreapp2.2\$rid\native\clrcompression.dll" -Verbose -Force
-}
-
 $archive_files = @()
 if($rid -eq "win-x64"){
-    $archive_files = @(".\NugetVendor\bin\Release\netcoreapp2.2\$rid\native\NugetVendor.exe",".\NugetVendor\bin\Release\netcoreapp2.2\$rid\native\clrcompression.dll")
+    $archive_files = @(".\NugetVendor\bin\Release\netcoreapp2.2\$rid\native\NugetVendor.exe",".\NugetVendor\bin\Release\netcoreapp2.2\$rid\publish\clrcompression.dll")
 }else{
     $archive_files = @(".\NugetVendor\bin\Release\netcoreapp2.2\$rid\native\NugetVendor")
 }
